97512f274473aa8887a1e9757ec1ef097ac2363d
Stefan Lundberg <lundberg.stefan@gmail.com>
2014-09-01 13:49:02 +0200

    Removed no usable properties in mockmodel.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -5,8 +5,45 @@ describe('service', function() {
   // load modules
   beforeEach(module('phonecatApp'));
 
-  // Test service availability
-  it('check the existence of Phone factory', inject(function(Phone) {
-      expect(Phone).toBeDefined();
+    var phone, cart;
+    var mockItems =  [
+        {
+            "age": 7,
+            "price": 234,
+            "carrier": "Cellular South",
+            "id": "lg-axis",
+            "imageUrl": "img/phones/lg-axis.0.jpg",
+            "name": "LG Axis",
+            "snippet": "Android Powered, Google Maps Navigation, 5 Customizable Home Screens"
+        },
+        {
+            "age": 3,
+            "price": 234,
+            "id": "dell-streak-7",
+            "imageUrl": "img/phones/dell-streak-7.0.jpg",
+            "name": "Dell Streak 7",
+            "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
+        }
+    ];
+    beforeEach(inject(function (Phone, Cart) {
+      phone = Phone;
+      cart = Cart;
     }));
+
+    it('check the existence of Phone factory', function() {
+        expect(phone).toBeDefined();
+    });
+
+    it('check the existence of Cart factory', function() {
+        expect(cart).toBeDefined();
+    });
+    
+    it('When adding to phones to Cart price should be updated', function() {
+       //TODO
+       cart.addItem(mockItems[0]);
+        console.log("", cart.totalAmount);
+        
+       
+    });
+
 });
