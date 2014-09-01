3610d4c9d367a6360749b928e6bbc03cb028b954
Stefan Lundberg <lundberg.stefan@gmail.com>
2014-09-01 13:28:04 +0200

    Removed no usable properties in mockmodel.

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -8,29 +8,11 @@ describe('directives', function () {
     beforeEach(module('templates'));
 
     var scope, element, ctrl,
-        items = [
-            {
-                "age": 7,
-                "price": 234,
-                "carrier": "Cellular South",
-                "id": "lg-axis",
-                "imageUrl": "img/phones/lg-axis.0.jpg",
-                "name": "LG Axis",
-                "snippet": "Android Powered, Google Maps Navigation, 5 Customizable Home Screens"
-            },
-            {
-                "age": 3,
-                "price": 234,
-                "id": "dell-streak-7",
-                "imageUrl": "img/phones/dell-streak-7.0.jpg",
-                "name": "Dell Streak 7",
-                "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
-            }
-        ];
+        mockItems = [{"name": "LG Axis"}, {"name": "Dell Streak 7"}];
 
     function addItemsToCart() {
-        for (var i = 0; i < items.length; i++) {
-            scope.cart.addItem(items[i]);
+        for (var i = 0; i < mockItems.length; i++) {
+            scope.cart.addItem(mockItems[i]);
         }
     };
 
@@ -52,7 +34,7 @@ describe('directives', function () {
 
     it('the table rows should equal the nr of items in the cart', function () {
         expect(element.find('tr').length).toEqual(scope.cart.items.length);
-        scope.cart.removeItem(items[0]);
+        scope.cart.removeItem(mockItems[0]);
         scope.$digest();
         expect(element.find('tr').length).toEqual(scope.cart.items.length);
     });
