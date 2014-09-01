ebc2ed3cca8a1ee74d37c71dae4fb3b1f40143d2
Stefan Lundberg <lundberg.stefan@gmail.com>
2014-09-01 12:44:15 +0200

    Refactored test.

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -7,16 +7,11 @@ describe('directives', function () {
     beforeEach(module('phonecatApp'));
     beforeEach(module('templates'));
 
-    var scope, element;
-
-    beforeEach(inject(function ($rootScope, $compile) {
-        element = angular.element("<drop-down-menu></drop-down-menu>");
-        scope = $rootScope;
-
-        scope.cart = {items : [
+    var scope, element, ctrl,
+        items = [
             {
                 "age": 7,
-                "price" : 234,
+                "price": 234,
                 "carrier": "Cellular South",
                 "id": "lg-axis",
                 "imageUrl": "img/phones/lg-axis.0.jpg",
@@ -25,17 +20,27 @@ describe('directives', function () {
             },
             {
                 "age": 3,
-                "price" : 234,
+                "price": 234,
                 "id": "dell-streak-7",
                 "imageUrl": "img/phones/dell-streak-7.0.jpg",
                 "name": "Dell Streak 7",
                 "snippet": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around."
             }
-        ]};
+        ];
+    function addItemsToCart() {
+        for (var i = 0; i < items.length; i++) {
+            scope.cart.addItem(items[i]);
+        }
+    };
 
+    beforeEach(inject(function ($rootScope, $compile, $controller) {
+        element = angular.element("<drop-down-menu></drop-down-menu>");
+        scope = $rootScope;
+        ctrl = $controller('PhoneCartCtrl', { $scope: scope });
+
+        addItemsToCart();
         $compile(element)(scope);
         scope.$digest();
-
     }));
 
     it('should contain the names of the items in the cart', function () {
@@ -44,7 +49,10 @@ describe('directives', function () {
         }
     });
 
-    it('the table rows should equal the nr of items in the cart', function() {
-       expect(element.find('tr').length).toEqual(scope.cart.items.length);
+    it('the table rows should equal the nr of items in the cart', function () {
+        expect(element.find('tr').length).toEqual(scope.cart.items.length);
+        scope.cart.removeItem(items[0]);
+        scope.$digest();
+        expect(element.find('tr').length).toEqual(scope.cart.items.length);
     });
 });
